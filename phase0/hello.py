1bc922beb6eed3b29120eb87c6adfc03acb309e7
Shaun Kean <skean2000@oultook.com>
2026-02-04 16:02:18 +0000

    response and failure

--- phase0/hello.py
+++ phase0/hello.py
@@ -1,1 +1,17 @@
-print('hello')
+import sys
+print(sys.version)
+
+import os
+
+results = []
+testdir = "C:\Test"
+for folder in testdir:
+  for f in os.listdir(folder):
+    if f.endswith('.pdf'):
+        results.append(f)
+
+print (results)
+
+
+file_name = __file__
+print(f'This file is located at: {file_name}')
