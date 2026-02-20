c48b3f972ac6a3e526dd21b087cabc6539ebf45d
Shaun Kean <skean2000@outlook.com>
2026-02-20 10:27:30 +0000

    fix code from GBT

--- phase1/functions_probe.py
+++ phase1/functions_probe.py
@@ -1,13 +1,11 @@
-import os
 import sys
 from pathlib import Path
 
 def describe_environment():
     return {
-        "python_executable\n": sys.executable,
-        "working_directory\n": os.getcwd(),
-        "files_in_working_directory\n": [f.name for f in Path.cwd().iterdir() if f.is_file()],
-        "file_in_current work directory (list)\n": [f.name for f in Path.cwd().iterdir() if f.is_file()]
+        "python_executable": sys.executable,
+        "current_working_directory": str(Path.cwd()),
+        "script_directory": str(Path(__file__).parent),
+        "files_in_current_directory": [f.name for f in Path.cwd().iterdir() if f.is_file()],
     }
-
 print(describe_environment())
