a8f5862d103db2c6f2d7f78559b5c4abd305fef3
Shaun Kean <skean2000@outlook.com>
2026-03-31 11:24:48 +0100

    working but only for the first person and it need to have a better layout

--- phase2/processor.py
+++ phase2/processor.py
@@ -1,6 +1,8 @@
 def load_records():
     with open("records.txt") as f:
         lines = f.readlines()
+        lines = lines[0]
         print(lines)
-
+        print(lines.strip().split(","))
+        
 load_records()
