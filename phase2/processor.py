405b53a70c6b5cf7b19aa0376072e0ae708fa5d7
Shaun Kean <skean2000@outlook.com>
2026-04-01 11:39:27 +0100

    getting the first person name, aga and role printed

--- phase2/processor.py
+++ phase2/processor.py
@@ -2,7 +2,10 @@ def load_records():
     with open("records.txt") as f:
         lines = f.readlines()
         lines = lines[0]
-        print(lines)
-        print(lines.strip().split(","))
-        
+        #print(lines)
+        parts = lines.strip().split(",") 
+        print(parts[0])
+        print(parts[1])
+        print(parts[2])
+
 load_records()
