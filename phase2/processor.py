51e7fc91517dea1db11ce8debaa1faa3747e7fa4
Shaun Kean <skean2000@outlook.com>
2026-04-02 11:18:41 +0100

    Done it, needed to change were the print was so it was inline of record

--- phase2/processor.py
+++ phase2/processor.py
@@ -1,7 +1,6 @@
 def load_records():
     with open("records.txt") as f:
         lines = f.readlines()
-        line = lines[0]
         for line in lines:
 
             parts = line.strip().split(",")
@@ -10,8 +9,8 @@ def load_records():
                 "name": parts[0],
                 "age": int(parts[1]),
                 "role": parts[2]
-        }
+            }
 
-        print(record)
+            print(record)
 
 load_records()
