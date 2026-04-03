7c2caed2c3d26694d6bad3ebf729fa2f3abc838e
Shaun Kean <skean2000@outlook.com>
2026-04-03 11:42:11 +0100

    moving onto group role for records

--- phase2/processor.py
+++ phase2/processor.py
@@ -1,5 +1,6 @@
 def load_records():
     record_list = []   # ← create list ONCE
+    role_list = []
 
     with open("records.txt") as f:
         lines = f.readlines()
@@ -12,10 +13,17 @@ def load_records():
                 "age": int(parts[1]),
                 "role": parts[2]
             }
-
             record_list.append(record)  # ← add to same list
 
     return record_list   # ← give the result back
 
 records = load_records()
 print(records)
+
+def group_by_role(records):
+    grouped = {}
+
+    for record in records:
+        print(record)
+
+group_by_role(records)
