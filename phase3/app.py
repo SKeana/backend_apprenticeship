03f3c402bdf049a7e09ca995c6afebdd7f31e757
Shaun Kean <skean2000@outlook.com>
2026-04-14 12:32:54 +0100

    clean up

--- phase3/app.py
+++ phase3/app.py
@@ -3,21 +3,21 @@ def load_log() -> list:
 
     with open("logs.txt") as f:
         lines = f.readlines()
-    
-    with open("logs.txt", "r") as f:
-        line = f.readline()
 
         for line in lines:
             roles = line.strip().split(",")
 
             group_comp = {
-                "player name": roles[0],
+                "player_name": roles[0],
                 "class": roles[1],
                 "role": roles[2],
                 "dps": int(roles[3])
             }
+
             comps.append(group_comp)
+
     return comps
+
 
 comps = load_log()
 print(comps)
